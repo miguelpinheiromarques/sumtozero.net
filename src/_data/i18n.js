76e889638fdf9bf775279650e9718863067da86e
miguelpinheiromarques <me@miguelpinheiromarques.com>
2025-11-22 13:10:11 +0000

    Add files via upload

---
+++ src/_data/i18n.js
@@ -0,0 +1,14 @@
+module.exports = {
+  home: {
+	en: "Home",
+	pt: "Inicio"
+  },
+  about: {
+	en: "About",
+	pt: "Sobre nosotros"
+  },
+  select_language: {
+	en: "Select Language",
+	pt: "Seleccionar idioma"
+  }
+};
