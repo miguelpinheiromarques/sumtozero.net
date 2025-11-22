1beab2873029f15cfe0826c43bd0f3f7383f398c
miguelpinheiromarques <me@miguelpinheiromarques.com>
2025-11-22 13:22:16 +0000

    Update i18n.js

--- src/_data/i18n.js
+++ src/_data/i18n.js
@@ -1,12 +1,16 @@
 module.exports = {
   home: {
 	en: "Home",
-	pt: "Inicio"
+	pt: "Início"
   },
   about: {
 	en: "About",
 	pt: "Sobre"
   },
+  contact: {
+	en: "Contact",
+	pt: "Contacto"
+  },	
   select_language: {
 	en: "Select Language",
 	pt: "Selecionar idioma"
